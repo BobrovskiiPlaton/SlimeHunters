7a3256992a9a9fc2c5f0e5a297abd3f050fbae73
KreKer_ <platon.bob@gmail.com>
2024-07-03 15:22:57 +0300

    multiplayer (hopefully lmao)

--- SlimeHunters/Assets/Scripts/PlayerController.cs
+++ SlimeHunters/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float _speed;
     public Transform player;
 
+    private PhotonView view;
+
     // Update is called once per frame
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        view = GetComponent<PhotonView>();
     }
 
     void FixedUpdate()
@@ -20,10 +24,14 @@ public class PlayerController : MonoBehaviour
 
     void GetInput()
     {
-        Vector3 inputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Jump"));
-        inputDir.Normalize();
-        Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x + transform.up * inputDir.z) * _speed;
-        transform.position += velocity * Time.deltaTime; 
+        if (view.IsMine)
+        {
+            Vector3 inputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Jump"));
+            inputDir.Normalize();
+            Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x + transform.up * inputDir.z) * _speed;
+            transform.position += velocity * Time.deltaTime;  
+        }
+        
     }
 
 }
