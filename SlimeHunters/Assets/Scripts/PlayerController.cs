9a38d5c0fe80d6d741bdd7e4b5ca2001596ffd87
KreKer_ <platon.bob@gmail.com>
2024-07-03 15:57:57 +0300

    PlayerController Update

--- SlimeHunters/Assets/Scripts/PlayerController.cs
+++ SlimeHunters/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,23 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float _speed;
     public Transform player;
-
+    public Camera camera;
     private PhotonView view;
+    
+    
+    //Для камеры
+    private Quaternion StartingRotation;
+    private float inputX;
+    private float inputY;
+    public float sensitivity;
 
     // Update is called once per frame
     void Start()
     {
+        StartingRotation = transform.rotation;
+
         Cursor.lockState = CursorLockMode.Locked;
+        camera = GetComponentInChildren<Camera>();
         view = GetComponent<PhotonView>();
     }
 
@@ -29,7 +39,15 @@ public class PlayerController : MonoBehaviour
             Vector3 inputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Jump"));
             inputDir.Normalize();
             Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x + transform.up * inputDir.z) * _speed;
-            transform.position += velocity * Time.deltaTime;  
+            transform.position += velocity * Time.deltaTime;
+            
+            inputX += Input.GetAxis("Mouse X") * sensitivity;
+            inputY += Input.GetAxis("Mouse Y") * sensitivity;
+            inputY = Mathf.Clamp(inputY, -90f, 90f);
+            Quaternion RotX = Quaternion.AngleAxis(inputX, Vector3.up);
+            Quaternion RotY = Quaternion.AngleAxis(-inputY, Vector3.right);
+            player.transform.rotation = StartingRotation * RotX;
+            camera.transform.rotation = StartingRotation * RotX * RotY;
         }
         
     }
